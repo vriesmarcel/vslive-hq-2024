908fc9437259f1618d2ce906e741b262a97c208f
Marcel de Vries <vriesmarcel@hotmail.com>
2025-02-16 23:34:00 +0100

    add remote setup services and upgrte to .net 8

---
+++ Tests.Playwright/PlawrightServiceSetup.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Azure.Developer.MicrosoftPlaywrightTesting.NUnit;
+
+
+namespace Tests.Playwright
+{
+
+    [SetUpFixture]
+    public class PlaywrightServiceSetup : PlaywrightServiceNUnit { };
+}
